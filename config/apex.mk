PixelExperience/vendor_aosp

vendor: Import apex from RD1A.201105.003.C1
Change-Id: Ic74069324343cee7cece4a7a182cf76f35147d3c

config/apex.mk
@@ -1,0 +1,26 @@
+# Enable Google Play system updates support
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/aosp/apex
+
+# ModuleMetadata
+PRODUCT_PACKAGES += \
+    ModuleMetadataGoogle
+
+# Google Apexes
+PRODUCT_PACKAGES += \
+    com.google.android.adbd \
+    com.google.android.cellbroadcast \
+    com.google.android.conscrypt \
+    com.google.android.extservices \
+    com.google.android.ipsec \
+    com.google.android.media \
+    com.google.android.media.swcodec \
+    com.google.android.mediaprovider \
+    com.google.android.neuralnetworks \
+    com.google.android.os.statsd \
+    com.google.android.permission \
+    com.google.android.resolv \
+    com.google.android.sdkext \
+    com.google.android.telephony \
+    com.google.android.tzdata2 \
+    com.google.android.wifi

config/common.mk
@@ -167,4 +167,7 @@ $(call inherit-product, vendor/aosp/config/carrier_settings.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
+# Apex
+$(call inherit-product, vendor/aosp/config/apex.mk)
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

AtomicXZ/vendor_potato

Signed-off-by: AtomicXZ <achubadyal4@gmail.com>

config/apex.mk
@@ -1,0 +1,26 @@
+# Enable Google Play system updates support
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/potato/apex
+
+# ModuleMetadata
+PRODUCT_PACKAGES += \
+    ModuleMetadataGoogle
+
+# Google Apexes
+PRODUCT_PACKAGES += \
+    com.google.android.adbd \
+    com.google.android.cellbroadcast \
+    com.google.android.conscrypt \
+    com.google.android.extservices \
+    com.google.android.ipsec \
+    com.google.android.media \
+    com.google.android.media.swcodec \
+    com.google.android.mediaprovider \
+    com.google.android.neuralnetworks \
+    com.google.android.os.statsd \
+    com.google.android.permission \
+    com.google.android.resolv \
+    com.google.android.sdkext \
+    com.google.android.telephony \
+    com.google.android.tzdata2 \
+    com.google.android.wifi

config/common.mk
@@ -98,6 +98,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/potato/config/permissions/custom-power-whitelist.xml:system/etc/sysconfig/custom-power-whitelist.xml
 
+# Apex
+$(call inherit-product, vendor/potato/config/apex.mk)
+
 # Clang
 ifeq ($(TARGET_USE_LATEST_CLANG),true)
     TARGET_KERNEL_CLANG_VERSION := $(shell grep -v based prebuilts/clang/host/$(HOST_OS)-x86/*/AndroidVersion.txt | sort | tail -n 1 | cut -d : -f 2)