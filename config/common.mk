PixelExperience/vendor_aosp

vendor: Disable Google Play System Updates activity if apex disabled
Change-Id: If30c3b29e61026521078f0ed75ffb34ef8f8dcc7

config/common.mk
@@ -188,7 +188,16 @@ PRODUCT_PRODUCT_PROPERTIES += \
 #endif
 
 # Apex
+ifeq ($(TARGET_FLATTEN_APEX),false)
 $(call inherit-product, vendor/aosp/config/apex.mk)
+else
+# Hide "Google Play System Updates" if Apex disabled
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/aosp/overlay_apex_disabled
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay_apex_disabled/common
+endif
 
 # Audio
 $(call inherit-product, vendor/aosp/config/audio.mk)

AtomicXZ/vendor_potato

Signed-off-by: AtomicXZ <achubadyal4@gmail.com>

config/common.mk
@@ -99,7 +99,16 @@ PRODUCT_COPY_FILES += \
     vendor/potato/config/permissions/custom-power-whitelist.xml:system/etc/sysconfig/custom-power-whitelist.xml
 
 # Apex
+ifeq ($(TARGET_FLATTEN_APEX),false)
 $(call inherit-product, vendor/potato/config/apex.mk)
+else
+# Hide "Google Play System Updates" if Apex disabled
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+   vendor/potato/overlay_apex_disabled
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/potato/overlay_apex_disabled/common
+endif
 
 # Clang
 ifeq ($(TARGET_USE_LATEST_CLANG),true)